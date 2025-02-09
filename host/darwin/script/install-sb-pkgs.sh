c579737519bd2e70117f22c3585dcda79e7062e5
curoky <cccuroky@gmail.com>
2025-02-09 09:02:56 +0800

    build: update package lists and VS Code AI tool allowlist
    Add smartmontools, cloc, and parallel to Darwin host install script
    Add python314 and nodejs-slim26 to base image pkgs_nolink list
    Comment out redundant smartmontools/parallel entries in personal Brewfile
    Expand VS Code AI toolcall allowlist with additional utility commands

--- host/darwin/script/install-sb-pkgs.sh
+++ host/darwin/script/install-sb-pkgs.sh
@@ -1,20 +1,4 @@
 #!/usr/bin/env bash
-# Copyright (c) 2018-2025 curoky(cccuroky@gmail.com).
-#
-# This file is part of devspace.
-# See https://github.com/curoky/devspace for further info.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 set -xeuo pipefail
 
@@ -70,6 +54,11 @@ pkgs=(
   # git
   git-filter-repo
   biome
+  smartmontools
+  cloc
+  parallel
+  exiftool
+  wget
 
   # lxgw-wenkai
   # fira-code
@@ -82,7 +71,6 @@ pkgs=(
   # aria2
   # gost
 
-  # ruff need link jemalloc
 )
 
 sudo mkdir -p /opt/sb
